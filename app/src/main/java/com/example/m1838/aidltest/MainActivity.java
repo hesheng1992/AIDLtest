b028fc993877819bffefd6271f52addb25f76fde
m1838 <1076390182@qq.com>
2017-06-19 14:28:16 +0800

    第一次提交整个项目

---
+++ app/src/main/java/com/example/m1838/aidltest/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.m1838.aidltest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
